91af99a569a1aeb485db6653b4b7bab0ef0ac01f
pratikksahu <pratiksahu184@gmail.com>
2021-02-18 12:07:55 +0530

    Modified testing for images in folder

--- image.py
+++ image.py
@@ -6,8 +6,8 @@ import cv2
 import os
 
 ap = argparse.ArgumentParser()
-ap.add_argument("-i", "--image", required=True,
-                help="path to input image")
+ap.add_argument("-f", "--folder", required=True,
+                help="path to input folder")
 args = vars(ap.parse_args())
 
 print("[INFO] loading face detector...")
@@ -23,62 +23,65 @@ recognizer = pickle.loads(
 le = pickle.loads(
     open("output{}label.pickle".format(os.sep), "rb").read())
 
-image = cv2.imread(args["image"])
-image = imutils.resize(image, width=600)
-(h, w) = image.shape[:2]
-# construct a blob from the image
-imageBlob = cv2.dnn.blobFromImage(
-    cv2.resize(image, (300, 300)), 1.0, (300, 300),
-    (104.0, 177.0, 123.0), swapRB=False, crop=False)
-# apply OpenCV's deep learning-based face detector to localize
-# faces in the input image
-detector.setInput(imageBlob)
-detections = detector.forward()
+conf = 0.6
+for root, dirs, files in os.walk(args["folder"]):
+    for file in files:
+        path = "{}{}{}".format(args["folder"],os.sep,file)
+        image = cv2.imread(path)
+        image = imutils.resize(image, width=600)
+        (h, w) = image.shape[:2]
+        # construct a blob from the image
+        imageBlob = cv2.dnn.blobFromImage(
+            cv2.resize(image, (300, 300)), 1.0, (300, 300),
+            (104.0, 177.0, 123.0), swapRB=False, crop=False)
+        # apply OpenCV's deep learning-based face detector to localize
+        # faces in the input image
+        detector.setInput(imageBlob)
+        detections = detector.forward()
+        for i in range(0, detections.shape[2]):
+            # extract the confidence (i.e., probability) associated with the
+            # prediction
+            confidence = detections[0, 0, i, 2]
+            # filter out weak detections
+            if confidence > conf:
+                # compute the (x, y)-coordinates of the bounding box for the
+                # face
+                box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
+                (startX, startY, endX, endY) = box.astype("int")
+                # extract the face ROI
+                face = image[startY:endY, startX:endX]
+                (fH, fW) = face.shape[:2]
+                # ensure the face width and height are sufficiently large
+                if fW < 20 or fH < 20:
+                    continue
+                faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96),
+                                                 (0, 0, 0), swapRB=True, crop=False)
+                embedder.setInput(faceBlob)
+                vec = embedder.forward()
+                # perform classification to recognize the face
+                proba = 0.0
+                name = ''
+                preds = recognizer.predict_proba(vec)[0]
+                for me in range(len(preds)):
+                    if (preds[me] > 0.5) and (le.classes_[me] == 'pratik'):
+                        print(preds[me])
+                        print(le.classes_[me])
+                        proba = preds[me]
+                        name = le.classes_[me]
 
-for i in range(0, detections.shape[2]):
-    # extract the confidence (i.e., probability) associated with the
-    # prediction
-    confidence = detections[0, 0, i, 2]
-    # filter out weak detections
-    if confidence > 0.3:
-        # compute the (x, y)-coordinates of the bounding box for the
-        # face
-        box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-        (startX, startY, endX, endY) = box.astype("int")
-        # extract the face ROI
-        face = image[startY:endY, startX:endX]
-        (fH, fW) = face.shape[:2]
-        # ensure the face width and height are sufficiently large
-        if fW < 20 or fH < 20:
-            continue
-        faceBlob = cv2.dnn.blobFromImage(face, 1.0 / 255, (96, 96),
-                                         (0, 0, 0), swapRB=True, crop=False)
-        embedder.setInput(faceBlob)
-        vec = embedder.forward()
-        # perform classification to recognize the face
-        proba = 0.0
-        name = ''
-        preds = recognizer.predict_proba(vec)[0]
-        for me in range(len(preds)):
-            print(preds[me])
-            print(le.classes_[me])
-            if (preds[me] > 0.5) and (le.classes_[me] == 'pratik'):
-                proba = preds[me]
-                name = le.classes_[me]
-
-        #If couldn't find 'pratik'
-        if (proba == 0.0) and (name == ''):
-            j = np.argmax(preds)
-            proba = preds[j]
-            name = le.classes_[j]
-        # print(proba)
-        # print(name)
-        text = "{}: {:.2f}%".format(name, proba * 100)
-        y = startY - 10 if startY - 10 > 10 else startY + 10
-        cv2.rectangle(image, (startX, startY), (endX, endY),
-                      (0, 0, 255), 2)
-        cv2.putText(image, text, (startX, y),
-                    cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
-# show the output image
-cv2.imshow("Image", image)
-cv2.waitKey(0)
+                #If couldn't find 'pratik'
+                if (proba == 0.0) and (name == ''):
+                    j = np.argmax(preds)
+                    proba = preds[j]
+                    name = le.classes_[j]
+                # print(proba)
+                # print(name)
+                text = "{}: {:.2f}%".format(name, proba * 100)
+                y = startY - 10 if startY - 10 > 10 else startY + 10
+                cv2.rectangle(image, (startX, startY), (endX, endY),
+                              (0, 0, 255), 2)
+                cv2.putText(image, text, (startX, y),
+                            cv2.FONT_HERSHEY_SIMPLEX, 0.45, (0, 0, 255), 2)
+        # show the output image
+        cv2.imshow("Image", image)
+        cv2.waitKey(0)
