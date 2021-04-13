c9c130dcc298b42fb995ffbb79e7f7f312c91ce4
pratikksahu <pratiksahu184@gmail.com>
2021-04-13 23:15:43 +0530

    removed delay

--- Controller/Raspberry.py
+++ Controller/Raspberry.py
@@ -18,7 +18,7 @@ class Raspberry:
 
     def moveWheel(self):
         while not self.stopped:
-            sleep(self.delay)
+            # sleep(self.delay)
             if self._isFaceDetected:
                 if self._adjustWheel != None:                                        
                     self.moduleWheel.move(self._adjustWheel)      
@@ -27,7 +27,7 @@ class Raspberry:
                 
     def moveCamera(self):
         while not self.stopped:
-            sleep(self.delay)
+            # sleep(self.delay)
             if self._isFaceDetected:
                 if  self._adjustCamera != None:
                     pass
