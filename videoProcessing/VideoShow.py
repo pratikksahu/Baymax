df845b790d3018372c1ab33ad4601a953e5380d7
pratikksahu <pratiksahu184@gmail.com>
2021-04-15 17:56:33 +0530

    haarcascade face detection

--- videoProcessing/VideoShow.py
+++ videoProcessing/VideoShow.py
@@ -7,22 +7,20 @@ from datetime import datetime
 import os
 import pickle
 
+
 class VideoShow:
     """
     Class that continuously shows a frame using a dedicated thread.
     """
 
     def __init__(self, frame=None, frameInfo=FrameInfo(), classifier=None):
-        self.net = cv2.dnn.readNetFromCaffe(
-            'videoProcessing{}deploy.prototxt.txt'.format(os.sep), 'videoProcessing{}res10_300x300_ssd_iter_140000.caffemodel'.format(os.sep))
-        self.embedder = cv2.dnn.readNetFromTorch(
-            'videoProcessing{}openface.nn4.small2.v1.t7'.format(os.sep))
+        self.cascPath = "Classifier\haarcascade_frontalface_default.xml"
+        self.faceCascade = cv2.CascadeClassifier(self.cascPath)
 
         self.facePoint = FacePoint()
         self.frameInfo = frameInfo
         self.frame = frame
-        self.stopped = False
-        self.newFrame = frame
+        self.stopped = False        
         self.isFaceDetected = False
 
     def start(self):
@@ -31,65 +29,47 @@ class VideoShow:
 
     def show(self):
         while not self.stopped:
+            self.frame = cv2.cvtColor(self.frame, cv2.COLOR_BGR2GRAY)
+            faces = self.faceCascade.detectMultiScale(
+                self.frame,
+                scaleFactor=1.2,
+                minNeighbors=15,
+                minSize=(30, 30)
+            )
 
-            (h, w) = self.frame.shape[:2]
-            blob = cv2.dnn.blobFromImage(
-                self.frame, 1.0, (300, 300), (104.0, 177.0, 123.0))
-            self.net.setInput(blob)
-            detections = self.net.forward()
-
-            goodDetection = []
-            for i in range(0, detections.shape[2]):
-                if detections[0, 0, i, 2] > 0.5:
-                    goodDetection.append(detections[0, 0, i, 3:7])
-            
-            if len(goodDetection) > 0:
+            if len(faces) > 0:
                 self.isFaceDetected = True
             else:
                 self.isFaceDetected = False
 
-            for i in range(0, len(goodDetection)):
-                # extract the confidence (i.e., probability) associated with the
-                # prediction
-                # compute the (x, y)-coordinates of the bounding box for the
-                # object
-                box = goodDetection[i] * np.array([w, h, w, h])
-                (startX, startY, endX, endY) = box.astype("int")
-                X = int(startX)
-                Y = int(startY)
-                W = int(endX - startX)
-                H = int(endY - startY)
-                CX = int((startX + endX)/2)
-                CY = int((startY + endY)/2)
-
-                face = self.frame[startY:endY, startX:endX]
-                (fH, fW) = face.shape[:2]
-
-                # ensure the face width and height are sufficiently large
-                if fW < 20 or fH < 20:
-                    continue
-
-                self.facePoint = FacePoint(X, Y, W, H, CX, CY)
-                # draw the bounding box of the face along with the associated
-                # probability
-                cv2.rectangle(self.frame, (startX, startY),
-                              (endX, endY), (0, 0, 255), 2)
-                # X Axis
-                cv2.line(self.frame, (int((startX + endX)/2), int((startY+endY)/2)),
-                         (0, int((startY+endY)/2)), (0, 0, 255), 2)
-                # Y Axis
-                cv2.line(self.frame, (int((startX + endX)/2), int((startY+endY)/2)),
-                         (int((startX + endX)/2), 0), (0, 0, 255), 2)
-
-                cv2.putText(self.frame, ("X:{} Y:{} W:{} H:{}".format(
-                    X, Y, W, H)), (startX, startY-5), cv2.FONT_HERSHEY_SIMPLEX, 0.3, (0, 0, 255))
             # Draw Constraints in every frame irrespective of whether face has been detected or not
             cv2.putText(self.frame, ("Safe Area Line"), (self.frameInfo.frameWidthLimitL,
                                                          self.frameInfo.frameHeightLimitT - 5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255))
             cv2.rectangle(self.frame, (self.frameInfo.frameWidthLimitL, self.frameInfo.frameHeightLimitT),
                           (self.frameInfo.frameWidthLimitR, self.frameInfo.frameHeightLimitB), (255, 0, 0), 2)
-            goodDetection.clear()
-            self.newFrame = self.frame
+
+            # Draw a rectangle around the faces
+            for (x, y, w, h) in faces:
+                # (startx , starty) (endx , endy)
+                X = int(x)
+                Y = int(y)
+                W = int(w)
+                H = int(h)
+                CX = int((x+x+w)/2)
+                CY = int((y+y+h)/2)
+
+                self.facePoint = FacePoint(X, Y, W, H, CX, CY)
+                # Show Coordinates with width and height of face detected
+                cv2.putText(self.frame, ("X:{} Y:{} W:{} H:{}".format(
+                    x, y, w, h)), (x, y-5), cv2.FONT_HERSHEY_SIMPLEX, 0.6, (0, 0, 255))
+                cv2.rectangle(self.frame, (x, y), (x+w, y+h), (0, 255, 0), 2)
+                # X Axis
+                cv2.line(self.frame, (int(CX/2), int(CY/2)),
+                         (0, int(CY/2)), (0, 0, 255), 2)
+                # Y Axis
+                cv2.line(self.frame, (int(CX/2), int(CY/2)),
+                         (int(CX/2), 0), (0, 0, 255), 2)            
+        
             # cv2.imshow("Video", self.frame)
             # cv2.imshow("Gray" , gray)
 
