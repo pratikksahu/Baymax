397af86e539727a52109dc82e11900bc4116d0e2
pratikksahu <pratiksahu184@gmail.com>
2021-04-16 18:42:58 +0530

    f

--- Controller/moduleWheel.py
+++ Controller/moduleWheel.py
@@ -38,9 +38,9 @@ class Wheel:
         if direction == 'FORWARD':
             self.forward(100)
         elif direction == 'RIGHT':
-            self.right(20)
+            self.right(10)
         elif direction == 'LEFT':
-            self.left(20)
+            self.left(10)
         elif direction == 'BACKWARD':
             self.reverse()
         elif direction == 'NOMOV':
