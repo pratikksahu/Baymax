6f6b266dbdd641aa270ccb55a3ac06a268e55219
pratikksahu <pratiksahu184@gmail.com>
2021-05-11 17:22:57 +0530

    revert changes

--- Controller/moduleCamera.py
+++ Controller/moduleCamera.py
@@ -1,7 +1,9 @@
 import RPi.GPIO as GPIO
 from time import sleep
-from threading import Thread
+
 #Servo Pin 18
+# 160 > 18
+# -160 < 18
 
 VERTICALSERVO = 18
 
@@ -12,22 +14,25 @@ class Camera:
         GPIO.setmode(GPIO.BCM)
         GPIO.setup(VERTICALSERVO,GPIO.OUT)
         self.VS = GPIO.PWM(VERTICALSERVO,50)
-        self.angle = 0
+        self.oldAngle = 0
+        self.newAngle = 0
     
     def start(self):
         self.VS.start(0)
-        Thread(name='cameraModule' , target=self.setAngle()).start()
         return self
     
     def move(self , angle):
-        self.angle = angle
-        # self.setAngle(angle)
-
+        if not angle == 90:
+            self.newAngle = angle
+            if not (self.oldAngle == self.newAngle) :
+                self.setAngle(angle)
+                self.oldAngle = self.newAngle
+    
     def setAngle(self):
-        self.VS.ChangeDutyCycle(7+(self.angle/18))
-        sleep(0.2)
-        self.VS.ChangeDutyCycle(0)
+        self.VS.ChangeDutyCycle(2+(angle/18))
         sleep(0.3)
+        self.VS.ChangeDutyCycle(0)
+        sleep(1)
     
     def stop(self):
         self.VS.ChangeDutyCycle(0)
