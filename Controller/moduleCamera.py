8b66ef6447ff7a319df28c4227d05412b60f0531
pratikksahu <pratiksahu184@gmail.com>
2021-05-12 23:36:45 +0530

    changed default angle

--- Controller/moduleCamera.py
+++ Controller/moduleCamera.py
@@ -18,10 +18,8 @@ class Camera:
 
     def start(self):
         self.VS.start(0)
-        self.VS.ChangeDutyCycle(5)
-        sleep(0.1)
-        self.VS.ChangeDutyCycle(0)
-        sleep(0.1)
+        self.VS.ChangeDutyCycle(4)        
+        self.VS.ChangeDutyCycle(0)        
         Thread(name='setAngle' , target=self.setAngle).start()
         return self
 
@@ -31,7 +29,7 @@ class Camera:
     
     def setAngle(self):        
         while not self.stopped:                            
-            self.VS.ChangeDutyCycle(5+(self.angle/18))
+            self.VS.ChangeDutyCycle(4+(self.angle/18))
             sleep(0.2)
             self.VS.ChangeDutyCycle(0)
             sleep(0.3) 
