3bde110db8181c1a20f98c5aa48562a61cf19e86
pratikksahu <pratiksahu184@gmail.com>
2021-06-08 11:31:03 +0530

    Configured Calendar Api

--- alexa.py
+++ alexa.py
@@ -22,6 +22,10 @@ from flask import Flask , make_response , redirect , request , url_for
 from flask_ask import Ask, request, session, question, statement
 import click
 import math
+from googleapiclient.discovery import build
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from google.oauth2.credentials import Credentials
 
 
 
@@ -56,6 +60,8 @@ manual_mode = 0
 STATUSON = ['on','high']
 STATUSOFF = ['off','low']
 wheel = Wheel().start()
+events= None
+
 
 @app_video.route("/")
 def index():
@@ -297,6 +303,58 @@ def followDurationIntent(duration, room):
     else:
         return statement("Please disable manual mode first")    
 
+@ask.intent('calender_event')
+def calender_eventIntent():
+    global events
+    e=""
+    if not events:
+        print('No upcoming events found.')
+        e="No upcoming events found."
+        return statement(e)    
+
+    for event in events:
+        start = event['start'].get('dateTime', event['start'].get('date'))
+        print(start, event['summary'])
+        e= e+event['summary']+"\n"
+    
+    return statement(e)
+
+
+
+
+def fetch_event():
+    global events
+    creds = None
+    SCOPES = ['https://www.googleapis.com/auth/calendar.readonly']
+    # The file token.json stores the user's access and refresh tokens, and is
+    # created automatically when the authorization flow completes for the first
+    # time.
+    if os.path.exists('token.json'):
+        creds = Credentials.from_authorized_user_file('token.json', SCOPES)
+    # If there are no (valid) credentials available, let the user log in.
+    if not creds or not creds.valid:
+        if creds and creds.expired and creds.refresh_token:
+            creds.refresh(Request())
+        else:
+            flow = InstalledAppFlow.from_client_secrets_file(
+                'credentials.json', SCOPES)
+            creds = flow.run_local_server(port=0)
+        # Save the credentials for the next run
+        with open('token.json', 'w') as token:
+            token.write(creds.to_json())
+
+    service = build('calendar', 'v3', credentials=creds)
+
+    # Call the Calendar API
+    while True:
+        
+        now = datetime.utcnow().isoformat() + 'Z' # 'Z' indicates UTC time
+        print('Getting the upcoming 10 events')
+        events_result = service.events().list(calendarId='primary', timeMin=now,
+                                        maxResults=10, singleEvents=True,
+                                        orderBy='startTime').execute()
+        events = events_result.get('items', [])
+        sleep(20)
 
 
 @ask.intent('AMAZON.FallbackIntent')
@@ -313,7 +371,7 @@ if __name__ == '__main__':
             app.config['ASK_VERIFY_REQUESTS'] = False
             app_video.config['ASK_VERIFY_REQUESTS'] = False
     setIp()
-    # Thread(target=follow_face, args=[100]).start()
+    Thread(target=fetch_event).start()
     server_flask = Thread(target=start_flask)
     video_flask = Thread(target=start_flask_video, args=(getIp(),))
 
